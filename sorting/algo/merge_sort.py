d33d32caa0f39a1b4df057548042ef655ddc3f5e
vikas <vikas.koppula@outlook.com>
2021-08-14 02:44:17 -0400

    Applied fix to merge_sort.py

--- sorting/algo/merge_sort.py
+++ sorting/algo/merge_sort.py
@@ -1,5 +1,6 @@
 def mergesort(input_array: list):
     def mergesort_helper(arr: list, start: int, end: int):
+
         if start >= end:
             return
         # Divide the arr into two parts
@@ -11,10 +12,11 @@ def mergesort(input_array: list):
         # j will represent the first index of the right array
         i = start
         j = mid + 1
+        aux_arr: list = list()
         # define an aux array which is used to hold the merged and sorted elements
-        aux_arr: list = list() # the length would be (end - start) / 2
+        # the length would be (end - start) / 2
         while i <= mid and j <= end:
-            if arr[i] < arr[j]:
+            if arr[i] <= arr[j]:
                 aux_arr.append(arr[i])
                 i += 1
             else:
@@ -29,7 +31,10 @@ def mergesort(input_array: list):
             j += 1
         # copy back the to the original array
         print('aux array:', aux_arr)
-        arr[start:end] = aux_arr
+        # NOTE: end + 1 is needed as slicing uses the num of elements and not the indexes. Hence last element would be
+        # index of the last element + 1 as indexes start from 0
+        arr[start:end + 1] = aux_arr
+        aux_arr = list()
 
     mergesort_helper(input_array, 0, len(input_array) - 1)
     print('Input array after sorting')
@@ -40,3 +45,4 @@ test_arr = [3, 1, 41, 59, 26, 53]
 print(test_arr)
 print('....................')
 mergesort(test_arr)
+print(test_arr)
