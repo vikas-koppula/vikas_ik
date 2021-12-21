848e4a7f9f14fcf8d4b080b4a7cc48059ebaf9af
vikas <pearlvikasjam@gmail.com>
2021-12-21 02:57:08 -0500

    Rearranged the recursive class code into folders

--- recursion/class/letter_case_permutation.py
+++ recursion/class/letter_case_permutation.py
@@ -4,19 +4,22 @@ def letter_case_permutation(test_str: str):
     result: list = []
 
     def helper(s: str, i: int, slate: list):
-        # Each left node is going to accumulate of this result list.
-        # Will reach left node at the last letter
+        # DFS approach. Each node is going to add a char to the slate and then either have one child or two child nodes
+        # Leaf node will have a str of the required length. Hence next recursive call will exit with the condition below
         if i == len(s):
             result.append("".join(slate))
             return
         # If digit then only one recursive call is made, as there can only be one worker below
         if s[i].isdigit():
-            helper(s, i+1, slate.append(s[i]))
+            slate.append(s[i])
+            helper(s, i+1, slate)
+            slate.pop()
         else:
             # Add small letter and send one node down
             slate.append(s[i].lower())
             helper(s, i+1, slate)
             slate.pop()
+            # Add upper letter and send one node down
             slate.append(s[i].upper())
             helper(s, i+1, slate)
             slate.pop()
@@ -24,6 +27,6 @@ def letter_case_permutation(test_str: str):
     return result
 
 
-test_str_1 = "ab"
+test_str_1 = "a1b2"
 print("test_str_1=a1b2")
 print('answer:', letter_case_permutation(test_str_1))
