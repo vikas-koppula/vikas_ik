8ee55b81677805790d6042ba7fed591ae9d4546f
vikaskoppula <pearlvikasjam@gmail.com>
2023-01-22 02:44:09 -0500

    Fixed the quick_sort.py logic to give correct results

--- sorting/algo/quick_sort.py
+++ sorting/algo/quick_sort.py
@@ -1,25 +1,37 @@
 import random
 
 
-def quicksort(arr:list):
-    def quicksort_helper(arr, st_idx: int, end_idx: int):
-        pivot_idx: int = random.choice(arr[st_idx: end_idx + 1])
+def quicksort(arr: list):
+
+    def helper(arr, st_idx: int, end_idx: int):
+        if st_idx >= end_idx:
+            return
+        # pivot_idx: int = random.choice(arr[st_idx: end_idx + 1])
+        pivot_idx: int = random.choice(range(st_idx, end_idx+1))
         pivot_elem: int = arr[pivot_idx]
+        print('pivot_idx:', pivot_idx)
+        print('pivot_elem:', pivot_elem)
+        # Swap the first element with the pivot element, thus the pivot is the first position
         arr[st_idx], arr[pivot_idx] = arr[pivot_idx], arr[st_idx]
-        # now pivot element is at the st_idx position
-        green_idx: int = st_idx + 1
-        orange_idx: int = st_idx + 1
-        while green_idx <= end_idx:
-            if arr[green_idx] >= pivot_elem:
-                green_idx += 1
-            elif arr[green_idx] < pivot_elem:
-                # swap green with orange to have a contiguous orange and green blocks
-                arr[orange_idx], arr[green_idx] = arr[green_idx], arr[orange_idx]
+        # Walk the green pointer from left to right, and create orange and green sections
+        orange_idx = st_idx
+        for green_idx in range(st_idx + 1, end_idx + 1):
+            if arr[green_idx] < pivot_elem:
+                # Move orange pointer to next green element and then swap with the green pointer's element
                 orange_idx += 1
-                green_idx += 1
+                arr[green_idx], arr[orange_idx] = arr[orange_idx], arr[green_idx]
+        # Swap the pivot in the start position with the last element of the orange partition
+        # Pivot is now in its correct and final sorted position
+        arr[st_idx], arr[orange_idx] = arr[orange_idx], arr[st_idx]
+        # Call helper recursive call on the orange and green sections
+        helper(arr, st_idx, orange_idx-1)
+        helper(arr, orange_idx+1, end_idx)
+        # Original array is now fully sorted in place
+    helper(arr, 0, len(arr)-1)
+    return arr
 
 
-test_arr = [3, 1, 41, 59, 26, 53]
+test_arr = [5, 7, 6, 3, 1, 2, 4]
 print(test_arr)
 print('....................')
 quicksort(test_arr)
