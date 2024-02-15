ca62114c713e55c7fe1439c40ba8136483deca02
vikaskoppula <pearlvikasjam@gmail.com>
2024-02-15 14:23:22 -0500

    Added or modified code

--- sorting/007_merge_sorted_array.py
+++ sorting/007_merge_sorted_array.py
@@ -27,5 +27,52 @@ The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 """
 from typing import List
+
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        w = len(nums1)-1
+        i = m - 1
+        j = n - 1
+        if m == 0:
+            nums1[0] = nums2[0]
+        while w >= 0 and j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[w] = nums1[i]
+                w -= 1
+                i -= 1
+            else:
+                nums1[w] = nums2[j]
+                w -= 1
+                j -= 1
+
+
+sol = Solution()
+
+print('\n.........Test_Case_1...........')
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+print('Merge Sort:', sol.merge(nums1, m, nums2, n))
+
+print('\n.........Test_Case_2...........')
+nums1 = [1]
+m = 1
+nums2 = []
+n = 0
+print('Merge Sort:', sol.merge(nums1, m, nums2, n))
+
+print('\n.........Test_Case_3...........')
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+print('Merge Sort:', sol.merge(nums1, m, nums2, n))
+
+print('\n.........Test_Case_4...........')
+nums1 = [2,0]
+m = 1
+nums2 = [1]
+n = 1
+print('Merge Sort:', sol.merge(nums1, m, nums2, n))
