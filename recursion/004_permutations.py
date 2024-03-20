3442956f5fd65f2c0fd9dd1b58ce97e587b15b7c
vikaskoppula <pearlvikasjam@gmail.com>
2024-03-20 17:34:01 -0400

    Added or modified code

---
+++ recursion/004_permutations.py
@@ -0,0 +1,73 @@
+"""
+46. Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+"""
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result: List[List[int]] = list()
+
+        def helper(arr: List[int], i:int, slate: List[int]) -> None:
+            # Base case: Leaf node. One permutation reached
+            if i == len(arr):
+                result.append(slate[:])
+                return
+            # Loop is used to iterate through the possible elements that can be placed at the position i
+            for tmp in range(i, len(arr)):
+                # Swap i and tmp to set the tmp element at i
+                arr[i], arr[tmp] = arr[tmp], arr[i]
+                slate.append(arr[i])
+                helper(arr, i+1, slate)
+                slate.pop()
+                arr[i], arr[tmp] = arr[tmp], arr[i]
+
+        helper(nums, 0, [])
+        return result
+
+    # Dont use array copy based solution
+    def permute_no_swap(self, input_arr: list):
+
+        result: List[List[int]] = list()
+
+        def helper(arr: list, slate: List[int]):
+            if len(slate) == len(input_arr) or len(arr) == 0:
+                result.append(slate[:])
+                return
+            for i in range(0, len(arr)):
+                slate.append(arr[i])
+                helper(arr[0:i] + arr[i+1:len(arr)], slate)
+                slate.pop()
+
+        helper(input_arr, [])
+        return result
+
+
+sol = Solution()
+
+print('.........Test_Case_1...........')
+nums = [1, 2, 3]
+print("nums:", nums)
+print('Permutations:\n', sol.permute(nums))
+print('.........Test_Case_2...........')
+nums = [0, 1]
+print("nums:", nums)
+print('Permutations:\n', sol.permute(nums))
+print('.........Test_Case_3...........')
+nums = [1]
+print("nums:", nums)
+print('Permutations:\n', sol.permute(nums))
+print('.........Test_Case_4...........')
+nums = []
+print("nums:", nums)
+print('Permutations:\n', sol.permute(nums))
--- recursion/005_permutations_2.py
+++ recursion/005_permutations_2.py
@@ -11,15 +11,40 @@ Example 2:
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 """
-from typing import List
+from typing import List, Set
 
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result: List[List[int]] = list()
 
+        def helper(arr:List[int], i:int, slate:List[int]) -> None:
+            element_set: Set[int] = set()
+            if i == len(arr):
+                result.append(slate[:])
+                return
+            for tmp in range(i, len(arr)):
+                if arr[tmp] in element_set:
+                    continue
+                else:
+                    element_set.add(arr[tmp])
+                arr[i], arr[tmp] = arr[tmp], arr[i]
+                slate.append(arr[i])
+                helper(arr, i+1, slate)
+                slate.pop()
+                arr[i], arr[tmp] = arr[tmp], arr[i]
+
+        helper(nums, 0, [])
+        return result
 
 
 sol = Solution()
+
+print('.........Test_Case_1...........')
 nums = [1,1,2]
 print("nums:", nums)
-print("sol:", sol.permuteUnique(nums))
+print('Permutations:\n', sol.permuteUnique(nums))
+print('.........Test_Case_2...........')
+nums = [1,2,3]
+print("nums:", nums)
+print('Permutations:\n', sol.permuteUnique(nums))
