68f76750e88da36a97348e29876978a5b3a8fc9a
vikaskoppula <pearlvikasjam@gmail.com>
2024-03-20 01:56:55 -0400

    Added or modified code

--- recursion/permutations.py
+++ recursion/permutations.py
@@ -1,8 +1,15 @@
 """
-Given an array nums of distinct integers, return all the possible permutations.
-You can return the answer in any order.
+46. Permutations
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order
+Example 1:
 Input: nums = [1,2,3]
 Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]]
 """
 from typing import List
 
@@ -38,7 +45,7 @@ def permutations_no_swap(input_arr: list):
             return
         for i in range(0, len(arr)):
             slate.append(arr[i])
-            helper(arr[0:i] + arr[i+1:len(arr)+1], slate)
+            helper(arr[0:i] + arr[i+1:len(arr)], slate)
             slate.pop()
 
     helper(input_arr, [])
