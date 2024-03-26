d912c92eb772fd4d68065963cb395b7b78e083b7
vikaskoppula <pearlvikasjam@gmail.com>
2024-03-26 02:40:59 -0400

    Added or modified code

--- recursion/004_letter_combinations_phone_num.py
+++ recursion/004_letter_combinations_phone_num.py
@@ -11,6 +11,7 @@ Example 2:
 
 Input: digits = ""
 Output: []
+
 Example 3:
 Input: digits = "2"
 Output: ["a","b","c"]
@@ -19,3 +20,39 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        result: List[str] = list()
+        mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7':'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+        def helper(arr: str, i:int, slate: List[str]) -> None:
+            if len(digits) == 0:
+                return
+            if i == len(digits):
+                result.append("".join(slate))
+                return
+
+            for let in mapping[digits[i]]:
+                slate.append(let)
+                helper(arr, i+1, slate)
+                slate.pop()
+
+        helper(digits, 0, [])
+        return result
+
+
+sol = Solution()
+print('\n.........Test_Case_1...........')
+digits = "23"
+print("digits:", digits)
+print('Output: ', sol.letterCombinations(digits))
+print('\n.........Test_Case_2...........')
+digits = ""
+print("digits:", digits)
+print('Output: ', sol.letterCombinations(digits))
+print('\n.........Test_Case_3...........')
+digits = "2"
+print("digits:", digits)
+print('Output: ', sol.letterCombinations(digits))
+print('\n.........Test_Case_4...........')
+digits = "3"
+print("digits:", digits)
+print('Output: ', sol.letterCombinations(digits))
