8317c2bf298bb3b2b3cc5b9bc947424ceef08baa
vikaskoppula <pearlvikasjam@gmail.com>
2024-04-25 08:59:10 -0400

    Added or modified code

--- trees/013_convert_sorted_array_binary_search_tree.py
+++ trees/013_convert_sorted_array_binary_search_tree.py
@@ -13,8 +13,30 @@ Input: nums = [1,3]
 Output: [3,1]
 Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
 """
+from util.display_tree import display
 from util.treenode import TreeNode, build_tree
 from typing import List, Optional
 
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+
+        def helper(arr: List[int], s: int, e: int) -> TreeNode:
+            if s > e:
+                return None
+            if s == e:
+                return TreeNode(None, None, arr[s])
+            mid = s + int((e-s)/2)
+            root = TreeNode(None, None, arr[mid])
+            root.left = helper(arr, s, mid-1)
+            root.right = helper(arr, mid+1, e)
+            return root
+        return helper(nums, 0, len(nums)-1)
+
+
+sol = Solution()
+print('.........Test_Case_1...........')
+input = [-10, -3, 0, 5, 9]
+root = sol.sortedArrayToBST(input)
+display(root)
+print('sortedArrayToBST:\n', sol.sortedArrayToBST(input))
