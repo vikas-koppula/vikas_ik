6d7fbcd0c9704397cbd47986588560ab0439a0e5
vikas-koppula <pearlvikasjam@gmail.com>
2025-03-31 03:15:19 -0400

    Added code and modified code

--- trees/004_bst_delete.py
+++ trees/004_bst_delete.py
@@ -1,9 +1,10 @@
 """
+450. Delete Node in a BST
 Given a root node reference of a BST and a key, delete the node with the given key in the BST.
 Return the root node reference (possibly updated) of the BST.
 Basically, the deletion can be divided into two stages:
-Search for a node to remove.
-If the node is found, delete the node.
+1) Search for a node to remove.
+2) If the node is found, delete the node.
 
 Example 1:
 Input: root = [5,3,6,2,4,null,7], key = 3
@@ -26,4 +27,51 @@ from util.treenode import TreeNode, build_tree
 from typing import Optional
 
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    # One step right and then always left
+    def successor(self, root: TreeNode) -> int:
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+
+    # One step left and then always right
+    def predecessor(self, root: TreeNode) -> int:
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+
+        # delete from the right subtree
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        # delete from the left subtree
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        # delete the current node
+        else:
+            # the node is a leaf
+            if not (root.left or root.right):
+                root = None
+            # The node is not a leaf and has a right child
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            # the node is not a leaf, has no right child, and has a left child
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+
+        return root
+
+sol = Solution()
+
+print('.........Test_Case_1...........')
+input = '[5,3,6,2,4,null,7]'
+key = 3
+display(build_tree(input))
+root = build_tree(input)
+print('inorderSuccessor: ', sol.deleteNode(root, key))
