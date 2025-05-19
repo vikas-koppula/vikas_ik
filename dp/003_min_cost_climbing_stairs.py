08f1872d1f996be470b7ab555f1d19b69a932ba3
vikas-koppula <pearlvikasjam@gmail.com>
2025-05-19 15:04:15 -0400

    Added code and modified code

--- dp/003_min_cost_climbing_stairs.py
+++ dp/003_min_cost_climbing_stairs.py
@@ -24,6 +24,29 @@ Explanation: You will start at index 0.
 - Pay 1 and climb one step to reach the top.
 The total cost is 6.
 """
+from typing import List
+
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Important: Need to add the cost 0 of the dest to the cost array. This helps with the coding.
+        cost.append(0)
+        n = len(cost) # Dest step is in the n-1 position. Min cost of dest step will be in the nth position in Mem
+        # Memory structure to store subproblem results.
+        mem = dict()
+        # Base cases. Ground step with cost of 0. 1st step will be the cost of itself
+        mem[0] = 0
+        mem[1] = cost[0]
+        # Recursive case.
+        for i in range(2, n+1):
+            mem[i] = cost[i-1] + min(mem[i-1], mem[i-2])
+        return mem[n]
+
+sol = Solution()
+print('\n.........Test_Case_1...........')
+cost = [10,15,20]
+print('climbStairs:', sol.minCostClimbingStairs(cost))
+
+print('\n.........Test_Case_2...........')
+cost = [1,100,1,1,1,100,1,1,100,1]
+print('climbStairs:', sol.minCostClimbingStairs(cost))
